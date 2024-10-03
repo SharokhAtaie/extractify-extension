b3790da9d8695cccbc7c1074485c85f39452599c
ch1y0w0 <mehdi404@protonmail.com>
2024-10-04 02:40:47 +0330

    Added Scope checkbox to filter results

--- popup.js
+++ popup.js
@@ -14,6 +14,40 @@ const copySvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fil
 
 const infoIcon = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v2h-2v-2zm0 4h2v6h-2v-6z" fill="#888"/></svg>`;
 
+// Get the checkbox element
+const checkbox = document.getElementById('scope-checkbox');
+
+// Add an event listener to detect changes
+checkbox.addEventListener('change', function() {
+    updateDisplay();
+});
+
+// Filter urls and endpoints based on scope
+function updateDisplay() {
+  const urlsList = document.getElementById("url-list");
+  const endpointsList = document.getElementById("endpoint-list");
+
+  // Clear the lists
+  urlsList.innerHTML = '';
+  endpointsList.innerHTML = '';
+
+  const isChecked = checkbox.checked;
+
+  // Filter and display URLs
+  const filteredURLs = isChecked
+    ? allURLs.filter(url => getDomainFromUrl(url.text) === currentDomain)
+    : allURLs;
+
+  // Filter and display endpoints based on the filename domain
+  const filteredEndpoints = isChecked
+    ? allEndpoints.filter(endpoint => getDomainFromUrl(endpoint.filename) === currentDomain)
+    : allEndpoints;
+
+  updateList(urlsList, filteredURLs);
+  updateList(endpointsList, filteredEndpoints);
+}
+
+
 // Utility function to extract the domain from a URL
 function getDomainFromUrl(url) {
   try {
